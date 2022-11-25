dd2ff6643e66f238ae7d580c09d7e6424a848ad6
mikechen66 <875661606@qq.com>
2022-11-25 16:36:36 +0800

    Add files via upload

--- vision/python/supervised-contrastive-learning.py
+++ vision/python/supervised-contrastive-learning.py
@@ -4,31 +4,71 @@ Author: [Khalid Salama](https://www.linkedin.com/in/khalid-salama-24403144/)
 Date created: 2020/11/30
 Last modified: 2020/11/30
 Description: Using supervised contrastive learning for image classification.
-"""
-"""
+
 ## Introduction
 
-[Supervised Contrastive Learning](https://arxiv.org/abs/2004.11362)
-(Prannay Khosla et al.) is a training methodology that outperforms
-supervised training with crossentropy on classification tasks.
+[Supervised Contrastive Learning](https://arxiv.org/abs/2004.11362)(Prannay 
+Khosla et al.) is a training methodology that outperforms supervised training 
+with crossentropy on classification tasks.
 
 Essentially, training an image classification model with Supervised Contrastive
 Learning is performed in two phases:
 
-1. Training an encoder to learn to produce vector representations of input images such
-that representations of images in the same class will be more similar compared to
-representations of images in different classes.
-2. Training a classifier on top of the frozen encoder.
+1.Training an encoder to learn to produce vector representations of input images 
+  such that representations of images in the same class will be more similar 
+  compared to representations of images in different classes.
+2.Training a classifier on top of the frozen encoder.
 
-Note that this example requires [TensorFlow Addons](https://www.tensorflow.org/addons),
+Note the example requires [TensorFlow Addons](https://www.tensorflow.org/addons),
 which you can install using the following command:
 
-```python
-pip install tensorflow-addons
-```
+$ pip install tensorflow-addons
 
-## Setup
+## Build the encoder model
+
+The encoder model takes the image as input and turns it into a 2048-dimensional
+feature vector.
+
+## Build the classification model
+
+The classification model adds a fully-connected layer on top of the encoder, plus 
+a softmax layer with the target classes.
+
+## Experiment 1: Train the baseline classification model
+
+In this experiment, a baseline classifier is trained as usual, i.e., the encoder 
+and the classifier parts are trained together as a single model to minimize the 
+crossentropy loss.
+
+## Experiment 1: Train the baseline classification model
+
+## Experiment 2: Use supervised contrastive learning
+
+In this experiment, the model is trained in two phases. In the first phase, the 
+encoder is pretrained to optimize the supervised contrastive loss, described in 
+[Prannay Khosla et al.](https://arxiv.org/abs/2004.11362).
+
+In the second phase, the classifier is trained using the trained encoder with 
+its weights freezed; only the weights of fully-connected layers with the softmax 
+are optimized.
+
+## Conclusion
+
+As shown in the experiments, using the supervised contrastive learning technique
+outperformed the conventional technique in terms of the test accuracy. Note that
+the same training budget (i.e., number of epochs) was given to each technique.
+Supervised contrastive learning pays off when the encoder involves a complex
+architecture, like ResNet, and multi-class problems with many labels. In addition, 
+large batch sizes and multi-layer projection heads improve its effectiveness. See 
+the [Supervised Contrastive Learning](https://arxiv.org/abs/2004.11362) paper for 
+more details.
+
+You can use the trained model hosted on the first link and and try the demo on the 
+second link as follows. 
+[Hugging Face Hub](https://huggingface.co/keras-io/supervised-contrastive-learning-cifar10)
+[Hugging Face Spaces](https://huggingface.co/spaces/keras-io/supervised-contrastive-learning).
 """
+
 
 import tensorflow as tf
 import tensorflow_addons as tfa
@@ -36,9 +76,8 @@ import numpy as np
 from tensorflow import keras
 from tensorflow.keras import layers
 
-"""
+
 ## Prepare the data
-"""
 
 num_classes = 10
 input_shape = (32, 32, 3)
@@ -51,9 +90,7 @@ print(f"x_train shape: {x_train.shape} - y_train shape: {y_train.shape}")
 print(f"x_test shape: {x_test.shape} - y_test shape: {y_test.shape}")
 
 
-"""
 ## Using image data augmentation
-"""
 
 data_augmentation = keras.Sequential(
     [
@@ -68,13 +105,8 @@ data_augmentation = keras.Sequential(
 # Setting the state of the normalization layer.
 data_augmentation.layers[0].adapt(x_train)
 
-"""
+
 ## Build the encoder model
-
-The encoder model takes the image as input and turns it into a 2048-dimensional
-feature vector.
-"""
-
 
 def create_encoder():
     resnet = keras.applications.ResNet50V2(
@@ -99,13 +131,8 @@ num_epochs = 50
 dropout_rate = 0.5
 temperature = 0.05
 
-"""
+
 ## Build the classification model
-
-The classification model adds a fully-connected layer on top of the encoder,
-plus a softmax layer with the target classes.
-"""
-
 
 def create_classifier(encoder, trainable=True):
 
@@ -128,13 +155,7 @@ def create_classifier(encoder, trainable=True):
     return model
 
 
-"""
 ## Experiment 1: Train the baseline classification model
-
-In this experiment, a baseline classifier is trained as usual, i.e., the
-encoder and the classifier parts are trained together as a single model
-to minimize the crossentropy loss.
-"""
 
 encoder = create_encoder()
 classifier = create_classifier(encoder)
@@ -146,21 +167,9 @@ accuracy = classifier.evaluate(x_test, y_test)[1]
 print(f"Test accuracy: {round(accuracy * 100, 2)}%")
 
 
-"""
 ## Experiment 2: Use supervised contrastive learning
 
-In this experiment, the model is trained in two phases. In the first phase,
-the encoder is pretrained to optimize the supervised contrastive loss,
-described in [Prannay Khosla et al.](https://arxiv.org/abs/2004.11362).
-
-In the second phase, the classifier is trained using the trained encoder with
-its weights freezed; only the weights of fully-connected layers with the
-softmax are optimized.
-
-### 1. Supervised contrastive learning loss function
-"""
-
-
+# 1.Supervised contrastive learning loss function
 class SupervisedContrastiveLoss(keras.losses.Loss):
     def __init__(self, temperature=1, name=None):
         super(SupervisedContrastiveLoss, self).__init__(name=name)
@@ -189,10 +198,7 @@ def add_projection_head(encoder):
     return model
 
 
-"""
-### 2. Pretrain the encoder
-"""
-
+# 2.Pretrain the encoder
 encoder = create_encoder()
 
 encoder_with_projection_head = add_projection_head(encoder)
@@ -207,9 +213,8 @@ history = encoder_with_projection_head.fit(
     x=x_train, y=y_train, batch_size=batch_size, epochs=num_epochs
 )
 
-"""
-### 3. Train the classifier with the frozen encoder
-"""
+
+# 3.Train the classifier with the frozen encoder
 
 classifier = create_classifier(encoder, trainable=False)
 
@@ -217,23 +222,3 @@ history = classifier.fit(x=x_train, y=y_train, batch_size=batch_size, epochs=num
 
 accuracy = classifier.evaluate(x_test, y_test)[1]
 print(f"Test accuracy: {round(accuracy * 100, 2)}%")
-
-"""
-We get to an improved test accuracy.
-"""
-
-"""
-## Conclusion
-
-As shown in the experiments, using the supervised contrastive learning technique
-outperformed the conventional technique in terms of the test accuracy. Note that
-the same training budget (i.e., number of epochs) was given to each technique.
-Supervised contrastive learning pays off when the encoder involves a complex
-architecture, like ResNet, and multi-class problems with many labels.
-In addition, large batch sizes and multi-layer projection heads
-improve its effectiveness. See the [Supervised Contrastive Learning](https://arxiv.org/abs/2004.11362)
-paper for more details.
-
-You can use the trained model hosted on [Hugging Face Hub](https://huggingface.co/keras-io/supervised-contrastive-learning-cifar10)
-and try the demo on [Hugging Face Spaces](https://huggingface.co/spaces/keras-io/supervised-contrastive-learning).
-"""
