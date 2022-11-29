3278e8574aa4e8d7462ce20630c4ab7432c5472e
mikechen66 <875661606@qq.com>
2022-11-29 09:28:40 +0800

    Add files via upload

--- vision/python/involution.py
+++ vision/python/involution.py
@@ -3,66 +3,63 @@ Title: Involutional neural networks
 Author: [Aritra Roy Gosthipaty](https://twitter.com/ariG23498)
 Date created: 2021/07/25
 Last modified: 2021/07/25
-Description: Deep dive into location-specific and channel-agnostic involution  kernels.
+Description: Deep dive into location-specific and channel-agnostic involution kernels.
 
 ## Introduction
 
-Convolution has been the basis of most modern neural networks for computer vision. A 
-convolution kernel is spatial-agnostic and channel-specific. Because of this, it isn't 
-able to adapt to different visual patterns with respect to different spatial locations. 
-Along with location-related problems, the receptive field of convolution creates 
-challenges with regard to capturing long-range spatial interactions.
+Convolution is the basis of most modern neural networks for computer vision. A convolution 
+kernel is spatial-agnostic and channel-specific. Due to the reason, it can not adapt to 
+different visual patterns with respect to different spatial locations. Along with location
+related problems, the receptive field creates challenges with regard to capturing long 
+range spatial interactions. To address the above issues, Li et. al. rethink the properties 
+of convolution in the paper - Involution: Inverting the Inherence of Convolution for Visual
+Recognition with the weblink as follows: 
 
-To address the above issues, Li et. al. rethink the properties of convolution in 
-[Involution: Inverting the Inherence of Convolution for VisualRecognition]
-(https://arxiv.org/abs/2103.06255). The authors propose the involution kernel, that is 
-location-specific and channel-agnostic. Due to the location-specific nature of the 
-operation, the authors say that self-attention falls under the design paradigm of 
-involution.
+https://arxiv.org/abs/2103.06255
 
-This example describes the involution kernel, compares two image classification models, 
-one with convolution and the other with involution, and also tries drawing a parallel 
-with the self-attention layer.
+The authors propose the involution kernel that is location-specific and channel agnostic. 
+Due to the location-specific nature of the operation, the authors say self-attention falls 
+under the design paradigm of involution. The example describes the involution kernel, 
+compares two image classification models, one with convolution and another with involution, 
+and tries drawing a parallel with the self-attention layer.
 
 ## Convolution
 
-Convolution remains the mainstay of deep neural networks for computer vision. To 
-understand Involution, it is necessary to talk about the convolution operation.
+Convolution remains the mainstay of deep neural networks for computer vision. To understand 
+Involution, it is necessary to talk about the convolution operation.
 
 ![Imgur](https://i.imgur.com/MSKLsm5.png)
 
-Consider an input tensor **X** with dimensions **H**, **W** and **C_in**. We take  a 
-collection of **C_out** convolution kernels each of shape **K**, **K**, **C_in**. With 
-the multiply-add operation between the input tensor and the kernels we obtain an output 
-tensor **Y** with dimensions **H**, **W**, **C_out**.
+Consider an input tensor X with dimensions H, W and C_in. We take  a collection of C_out 
+convolution kernels each of shape K, K, C_in. With the multiply-add operation between the 
+input tensor and the kernels we obtain an output tensor Y with dimensions H, W, C_out.
 
-In the diagram above `C_out=3`. This makes the output tensor of shape H, W and 3. One 
-can notice that the convoltuion kernel does not depend on the spatial position of the 
-input tensor which makes it **location-agnostic**. On the other hand, each channel in 
-the output tensor is based on a specific convolution filter which makes is**channel-
-specific**.
+In the diagram above C_out=3. It makes the output tensor of shape H, W and 3. One can notice 
+the convoltuion kernel does not depend on the spatial position of the input tensor which 
+makes it location-agnostic. On the other hand, each channel in the output tensor is based on 
+a specific convolution filter which makes is channel-specific.
 
 ## Involution
 
-The idea is to have an operation that is both **location-specific** and **channel-
-agnostic**. Trying to implement these specific properties poses a challenge. With a 
-fixed number of involution kernels (for each spatial position) we will **not** be 
-able to process variable-resolution input tensors.
+The idea is to have an operation that is both location-specific and channel-agnostic. Trying 
+to implement the specific properties poses a challenge. With a fixed number of involution 
+kernels (for each spatial position) we can not process variable-resolution input tensors.
 
-To solve this problem, the authors have considered *generating* each kernel conditioned 
-on specific spatial positions. With this method, we should be able to  process variable-
-resolution input tensors with ease. The diagram below provides an intuition on this 
-kernel generation method.
+To solve the problem, the authors considers generating each kernel conditioned on specific 
+spatial positions. With the method, we could process variable-resolution input tensors with 
+ease. The diagram below provides an intuition on this kernel generation method.
 
 ![Imgur](https://i.imgur.com/jtrGGQg.png)
 
 ## Image Classification
 
-In this section, we will build an image-classifier model. There will be two models one 
-with convolutions and the other with involutions.
+In this section, we will build an image-classifier model. There will be two models one with 
+convolutions and aother with involutions.
 
-The image-classification model is heavily inspired by this [Convolutional Neural Network 
-(CNN)](https://www.tensorflow.org/tutorials/images/cnn) tutorial from Google.
+The image-classification model is inspired by the paper - Convolutional Neural Network (CNN)] 
+tutorial from Google with the following weblink.
+
+https://www.tensorflow.org/tutorials/images/cnn
 
 ## Comparisons
 
@@ -70,49 +67,45 @@ In this section, we will be looking at both the models and compare a few pointer
 
 ### Parameters
 
-One can see that with a similar architecture the parameters in a CNN is much larger than 
-that of an INN (Involutional Neural Network).
+One can see that with a similar architecture the parameters in a CNN is much larger than that 
+of an INN (Involutional Neural Network).
 
 ### Loss and Accuracy Plots
 
-Here, the loss and the accuracy plots demonstrate that INNs are slow learners (with lower 
-parameters).
+The loss and the accuracy plots demonstrate that INNs are slow learners with lower parameters.
 
-## Visualizing Involution Kernels
+## Visualize Involution Kernels
 
-To visualize the kernels, we take the sum of **K×K** values from each involution kernel. 
-**All the representatives at different spatial locations frame the corresponding heat map.**
-
-The authors mention:
-
-"Our proposed involution is reminiscent of self-attention and essentially could become 
-a generalized version of it."
-
-With the visualization of the kernel we can indeed obtain an attention map of the image. 
-The learned involution kernels provides attention to individual spatial positions of the 
-input tensor. The **location-specific** property makes involution a generic space of 
-modelsin which self-attention belongs.
+To visualize the kernels, we take the sum of K×K values from each involution kernel. All the 
+representatives at different spatial locations frame the corresponding heat map.The authors 
+mention the proposed involution is reminiscent of self-attention and essentially could become 
+a generalized version of it. With the visualization of the kernel we can indeed obtain an 
+attention map of the image. The learned involution kernels provides attention to individual 
+spatial positions of the input tensor. The location-specific property makes involution a 
+generic space of modelsin which self-attention belongs.
 
 ## Conclusions
 
-In this example, the main focus was to build an `Involution` layer which can be easily 
-reused. While our comparisons were based on a specific task, feel free to use the layer 
-for different tasks and report your results.
+In theexample, the main focus was to build an Involution layer which can be easily reused. 
+While our comparisons were based on a specific task, feel free to use the layer for different 
+tasks and report your results.
 
-According to me, the key take-away of involution is its relationship with self-attention. 
-The intuition behind location-specific and channel-spefic processing makes sense in a lot 
-of tasks. Moving forward one can:
+The key take-away of involution is its relationship with self-attention. The intuition behind 
+location-specific and channel-spefic processing makes sense in a lot of tasks. Moving forward 
+one can take the following reference:
 
-- Look at [Yannick's video](https://youtu.be/pH2jZun8MoY) oF involution for a better 
-  understanding.
+- Look at [Yannick's video](https://youtu.be/pH2jZun8MoY) for a better understanding.
 - Experiment with the various hyperparameters of the involution layer.
 - Build different models with the involution layer.
 - Try building a different kernel generation method altogether.
 
-You can use the trained model hosted on [Hugging Face Hub]
-(https://huggingface.co/keras-io/involution)
-and try the demo on [Hugging Face Spaces]
-(https://huggingface.co/spaces/keras-io/involution).
+You can use the trained model hosted on the Hugging Face Hub with the following weblink. 
+
+https://huggingface.co/keras-io/involution)
+
+and try the demo on the Hugging Face Spaces
+
+https://huggingface.co/spaces/keras-io/involution
 """
 
 
@@ -128,8 +121,7 @@ tf.random.set_seed(42)
 
 class Involution(keras.layers.Layer):
     def __init__(
-        self, channel, group_number, kernel_size, stride, reduction_ratio, name
-    ):
+        self, channel, group_number, kernel_size, stride, reduction_ratio, name):
         super().__init__(name=name)
 
         # Initialize the parameters.
@@ -147,29 +139,22 @@ class Involution(keras.layers.Layer):
         height = height // self.stride
         width = width // self.stride
 
-        # Define a layer that average pools the input tensor
-        # if stride is more than 1.
+        # Define a layer that average pools the input tensor if 
+        # stride is more than 1.
         self.stride_layer = (
             keras.layers.AveragePooling2D(
-                pool_size=self.stride, strides=self.stride, padding="same"
-            )
+                pool_size=self.stride, strides=self.stride, padding="same")
             if self.stride > 1
-            else tf.identity
-        )
+            else tf.identity)
         # Define the kernel generation layer.
-        self.kernel_gen = keras.Sequential(
-            [
+        self.kernel_gen = keras.Sequential([
                 keras.layers.Conv2D(
-                    filters=self.channel // self.reduction_ratio, kernel_size=1
-                ),
+                    filters=self.channel // self.reduction_ratio, kernel_size=1),
                 keras.layers.BatchNormalization(),
                 keras.layers.ReLU(),
                 keras.layers.Conv2D(
                     filters=self.kernel_size * self.kernel_size * self.group_number,
-                    kernel_size=1,
-                ),
-            ]
-        )
+                    kernel_size=1,),])
         # Define reshape layers
         self.kernel_reshape = keras.layers.Reshape(
             target_shape=(
@@ -177,41 +162,33 @@ class Involution(keras.layers.Layer):
                 width,
                 self.kernel_size * self.kernel_size,
                 1,
-                self.group_number,
-            )
-        )
+                self.group_number,))
         self.input_patches_reshape = keras.layers.Reshape(
             target_shape=(
                 height,
                 width,
                 self.kernel_size * self.kernel_size,
                 num_channels // self.group_number,
-                self.group_number,
-            )
-        )
+                self.group_number,))
         self.output_reshape = keras.layers.Reshape(
-            target_shape=(height, width, num_channels)
-        )
+            target_shape=(height, width, num_channels))
 
     def call(self, x):
-        # Generate the kernel with respect to the input tensor.
+        # Generate the kernel with respect to the input tensor: 
         # B, H, W, K*K*G
         kernel_input = self.stride_layer(x)
         kernel = self.kernel_gen(kernel_input)
 
-        # reshape the kerenl
-        # B, H, W, K*K, 1, G
+        # Reshape the kerenl: B, H, W, K*K, 1, G
         kernel = self.kernel_reshape(kernel)
 
-        # Extract input patches.
-        # B, H, W, K*K*C
+        # Extract input patches: B, H, W, K*K*C
         input_patches = tf.image.extract_patches(
             images=x,
             sizes=[1, self.kernel_size, self.kernel_size, 1],
             strides=[1, self.stride, self.stride, 1],
             rates=[1, 1, 1, 1],
-            padding="SAME",
-        )
+            padding="SAME",)
 
         # Reshape the input patches to align with later operations.
         # B, H, W, K*K, C//G, G
@@ -223,8 +200,7 @@ class Involution(keras.layers.Layer):
         # B, H, W, C//G, G
         output = tf.reduce_sum(output, axis=3)
 
-        # Reshape the output kernel.
-        # B, H, W, C
+        # Reshape the output kernel: B, H, W, C
         output = self.output_reshape(output)
 
         # Return the output tensor and the kernel.
@@ -238,35 +214,27 @@ input_tensor = tf.random.normal((32, 256, 256, 3))
 
 # Compute involution with stride 1.
 output_tensor, _ = Involution(
-    channel=3, group_number=1, kernel_size=5, stride=1, reduction_ratio=1, name="inv_1"
-)(input_tensor)
+    channel=3, group_number=1, kernel_size=5, stride=1, reduction_ratio=1, name="inv_1")(input_tensor)
 print(f"with stride 1 ouput shape: {output_tensor.shape}")
 
 # Compute involution with stride 2.
 output_tensor, _ = Involution(
-    channel=3, group_number=1, kernel_size=5, stride=2, reduction_ratio=1, name="inv_2"
-)(input_tensor)
+    channel=3, group_number=1, kernel_size=5, stride=2, reduction_ratio=1, name="inv_2")(input_tensor)
 print(f"with stride 2 ouput shape: {output_tensor.shape}")
 
 # Compute involution with stride 1, channel 16 and reduction ratio 2.
 output_tensor, _ = Involution(
-    channel=16, group_number=1, kernel_size=5, stride=1, reduction_ratio=2, name="inv_3"
-)(input_tensor)
+    channel=16, group_number=1, kernel_size=5, stride=1, reduction_ratio=2, name="inv_3")(input_tensor)
 print(
-    "with channel 16 and reduction ratio 2 ouput shape: {}".format(output_tensor.shape)
-)
+    "with channel 16 and reduction ratio 2 ouput shape: {}".format(output_tensor.shape))
 
 
 ## Get the CIFAR10 Dataset
 
-# Load the CIFAR10 dataset.
 print("loading the CIFAR10 dataset...")
 (
     (train_images, train_labels),
-    (
-        test_images,
-        test_labels,
-    ),
+    (test_images,test_labels,),
 ) = keras.datasets.cifar10.load_data()
 
 # Normalize pixel values to be between 0 and 1.
@@ -276,8 +244,7 @@ print("loading the CIFAR10 dataset...")
 train_ds = (
     tf.data.Dataset.from_tensor_slices((train_images, train_labels))
     .shuffle(256)
-    .batch(256)
-)
+    .batch(256))
 test_ds = tf.data.Dataset.from_tensor_slices((test_images, test_labels)).batch(256)
 
 
@@ -293,8 +260,7 @@ class_names = [
     "frog",
     "horse",
     "ship",
-    "truck",
-]
+    "truck",]
 
 plt.figure(figsize=(10, 10))
 for i in range(25):
@@ -311,8 +277,7 @@ plt.show()
 
 # Build the conv model.
 print("building the convolution model...")
-conv_model = keras.Sequential(
-    [
+conv_model = keras.Sequential([
         keras.layers.Conv2D(32, (3, 3), input_shape=(32, 32, 3), padding="same"),
         keras.layers.ReLU(name="relu1"),
         keras.layers.MaxPooling2D((2, 2)),
@@ -323,17 +288,14 @@ conv_model = keras.Sequential(
         keras.layers.ReLU(name="relu3"),
         keras.layers.Flatten(),
         keras.layers.Dense(64, activation="relu"),
-        keras.layers.Dense(10),
-    ]
-)
+        keras.layers.Dense(10),])
 
 # Compile the mode with the necessary loss function and optimizer.
 print("compiling the convolution model...")
 conv_model.compile(
     optimizer="adam",
     loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-    metrics=["accuracy"],
-)
+    metrics=["accuracy"],)
 
 # Train the model.
 print("conv model training...")
@@ -346,19 +308,13 @@ conv_hist = conv_model.fit(train_ds, epochs=20, validation_data=test_ds)
 print("building the involution model...")
 
 inputs = keras.Input(shape=(32, 32, 3))
-x, _ = Involution(
-    channel=3, group_number=1, kernel_size=3, stride=1, reduction_ratio=2, name="inv_1"
-)(inputs)
+x, _ = Involution(channel=3, group_number=1, kernel_size=3, stride=1, reduction_ratio=2, name="inv_1")(inputs)
 x = keras.layers.ReLU()(x)
 x = keras.layers.MaxPooling2D((2, 2))(x)
-x, _ = Involution(
-    channel=3, group_number=1, kernel_size=3, stride=1, reduction_ratio=2, name="inv_2"
-)(x)
+x, _ = Involution(channel=3, group_number=1, kernel_size=3, stride=1, reduction_ratio=2, name="inv_2")(x)
 x = keras.layers.ReLU()(x)
 x = keras.layers.MaxPooling2D((2, 2))(x)
-x, _ = Involution(
-    channel=3, group_number=1, kernel_size=3, stride=1, reduction_ratio=2, name="inv_3"
-)(x)
+x, _ = Involution(channel=3, group_number=1, kernel_size=3, stride=1, reduction_ratio=2, name="inv_3")(x)
 x = keras.layers.ReLU()(x)
 x = keras.layers.Flatten()(x)
 x = keras.layers.Dense(64, activation="relu")(x)
@@ -371,10 +327,9 @@ print("compiling the involution model...")
 inv_model.compile(
     optimizer="adam",
     loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-    metrics=["accuracy"],
-)
+    metrics=["accuracy"],)
 
-# train the model
+# Train the model
 print("inv model training...")
 inv_hist = inv_model.fit(train_ds, epochs=20, validation_data=test_ds)
 
