2dea567305c3fc2c51ade18ba99ccdcfd7d500ae
mikechen66 <875661606@qq.com>
2022-11-30 21:43:49 +0800

    Add files via upload

--- RL/ppo_cartpole.py
+++ RL/ppo_cartpole.py
@@ -4,48 +4,45 @@ Author: [Ilias Chrysovergis](https://twitter.com/iliachry)
 Date created: 2021/06/24
 Last modified: 2021/06/24
 Description: Implementation of a Proximal Policy Optimization agent for the CartPole-v0 environment.
-"""
 
-"""
 ## Introduction
 
 This code example solves the CartPole-v0 environment using a Proximal Policy Optimization (PPO) agent.
 
-### CartPole-v0
+# CartPole-v0
 
-A pole is attached by an un-actuated joint to a cart, which moves along a frictionless track.
-The system is controlled by applying a force of +1 or -1 to the cart.
-The pendulum starts upright, and the goal is to prevent it from falling over.
-A reward of +1 is provided for every timestep that the pole remains upright.
-The episode ends when the pole is more than 15 degrees from vertical, or the cart moves more than 2.4 units from the center.
-After 200 steps the episode ends. Thus, the highest return we can get is equal to 200.
+A pole is attached by an un-actuated joint to a cart, which moves along a frictionless track. The system 
+is controlled by applying a force of +1 or -1 to the cart. The pendulum starts upright, and the goal is 
+to prevent it from falling over. A reward of +1 is provided for every timestep that the pole remains 
+upright. The episode ends when the pole is more than 15 degrees from vertical, or the cart moves more 
+than 2.4 units from the center. After 200 steps the episode ends. Thus, the highest return we can get is 
+equal to 200.
 
-[CartPole-v0](https://gym.openai.com/envs/CartPole-v0/)
+CartPole-v0: https://gym.openai.com/envs/CartPole-v0/)
 
-### Proximal Policy Optimization
+# Proximal Policy Optimization
 
-PPO is a policy gradient method and can be used for environments with either discrete or continuous action spaces.
-It trains a stochastic policy in an on-policy way. Also, it utilizes the actor critic method. The actor maps the
-observation to an action and the critic gives an expectation of the rewards of the agent for the observation given.
-Firstly, it collects a set of trajectories for each epoch by sampling from the latest version of the stochastic policy.
-Then, the rewards-to-go and the advantage estimates are computed in order to update the policy and fit the value function.
-The policy is updated via a stochastic gradient ascent optimizer, while the value function is fitted via some gradient descent algorithm.
-This procedure is applied for many epochs until the environment is solved.
+PPO is a policy gradient method and can be used for environments with either discrete or continuous action 
+spaces. It trains a stochastic policy in an on-policy way. Also, it utilizes the actor critic method. The 
+actor maps the observation to an action and the critic gives an expectation of the rewards of the agent for 
+the observation given. Firstly, it collects a set of trajectories for each epoch by sampling from the latest 
+version of the stochastic policy. Then, the rewards-to-go and the advantage estimates are computed in order 
+to update the policy and fit the value function. The policy is updated via a stochastic gradient ascent 
+optimizer, while the value function is fitted via some gradient descent algorithm. The procedure is applied 
+for many epochs until the environment is solved.
 
-![Algorithm](https://i.imgur.com/rd5tda1.png)
+Algorithm: https://i.imgur.com/rd5tda1.png
 
-- [PPO Original Paper](https://arxiv.org/pdf/1707.06347.pdf)
-- [OpenAI Spinning Up docs - PPO](https://spinningup.openai.com/en/latest/algorithms/ppo.html)
+- PPO Original Paper: https://arxiv.org/pdf/1707.06347.pdf
+- OpenAI Spinning Up docs - PPO: https://spinningup.openai.com/en/latest/algorithms/ppo.html
 
-### Note
+##  Note
 
-This code example uses Keras and Tensorflow v2. It is based on the PPO Original Paper,
-the OpenAI's Spinning Up docs for PPO, and the OpenAI's Spinning Up implementation of PPO using Tensorflow v1.
+This code example uses Keras and Tensorflow v2. It is based on the PPO Original Paper, the OpenAI's Spinning 
+Up docs for PPO, and the OpenAI's Spinning Up implementation of PPO using Tensorflow v1.
 
-[OpenAI Spinning Up Github - PPO](https://github.com/openai/spinningup/blob/master/spinup/algos/tf1/ppo/ppo.py)
-"""
+OpenAI Spinning Up Github - PPO: https://github.com/openai/spinningup/blob/master/spinup/algos/tf1/ppo/ppo.py
 
-"""
 ## Libraries
 
 For this example the following libraries are used:
@@ -54,7 +51,20 @@ For this example the following libraries are used:
 2. `tensorflow` and `keras` for building the deep RL PPO agent
 3. `gym` for getting everything we need about the environment
 4. `scipy.signal` for calculating the discounted cumulative sums of vectors
+
+## Visualizations
+
+Before training:
+![Imgur](https://i.imgur.com/rKXDoMC.gif)
+
+After 8 epochs of training:
+![Imgur](https://i.imgur.com/M0FbhF0.gif)
+
+After 20 epochs of training:
+![Imgur](https://i.imgur.com/tKhTEaF.gif)
 """
+
+
 import numpy as np
 import tensorflow as tf
 from tensorflow import keras
@@ -63,10 +73,8 @@ import gym
 import scipy.signal
 import time
 
-"""
-## Functions and class
-"""
 
+## Functions and class
 
 def discounted_cumulative_sums(x, discount):
     # Discounted cumulative sums of vectors for computing rewards-to-go and advantage estimates
@@ -196,9 +204,7 @@ def train_value_function(observation_buffer, return_buffer):
     value_optimizer.apply_gradients(zip(value_grads, critic.trainable_variables))
 
 
-"""
 ## Hyperparameters
-"""
 
 # Hyperparameters of the PPO algorithm
 steps_per_epoch = 4000
@@ -216,9 +222,8 @@ hidden_sizes = (64, 64)
 # True if you want to render the environment
 render = False
 
-"""
+
 ## Initializations
-"""
 
 # Initialize the environment and get the dimensionality of the
 # observation space and the number of possible actions
@@ -245,9 +250,9 @@ value_optimizer = keras.optimizers.Adam(learning_rate=value_function_learning_ra
 # Initialize the observation, episode return and episode length
 observation, episode_return, episode_length = env.reset(), 0, 0
 
-"""
+
 ## Train
-"""
+
 # Iterate over the number of epochs
 for epoch in range(epochs):
     # Initialize the sum of the returns, lengths and number of episodes for each epoch
@@ -313,19 +318,3 @@ for epoch in range(epochs):
     print(
         f" Epoch: {epoch + 1}. Mean Return: {sum_return / num_episodes}. Mean Length: {sum_length / num_episodes}"
     )
-
-"""
-## Visualizations
-
-Before training:
-
-![Imgur](https://i.imgur.com/rKXDoMC.gif)
-
-After 8 epochs of training:
-
-![Imgur](https://i.imgur.com/M0FbhF0.gif)
-
-After 20 epochs of training:
-
-![Imgur](https://i.imgur.com/tKhTEaF.gif)
-"""
